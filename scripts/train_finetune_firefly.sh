2482997781ca068edea1ded58c9dc4f20d697c6d
ticoAg <1627635056@qq.com>
2023-06-01 12:10:30 +0800

    update train_finetune_firefly.sh

--- scripts/train_finetune_firefly.sh
+++ scripts/train_finetune_firefly.sh
@@ -10,16 +10,17 @@ CUDA_VISIBLE_DEVICES=0 python src/train_pt.py \
     --finetuning_type $finetuning_type \
     --output_dir ckpt/$model-$finetuning_type-$dataset-$finetuning_type \
     --overwrite_cache \
-    --per_device_train_batch_size 4 \
+    --per_device_train_batch_size 8 \
     --gradient_accumulation_steps 4 \
     --lr_scheduler_type cosine \
     --logging_steps 10 \
-    --save_steps 1000 \
+    --save_steps 500 \
     --learning_rate 5e-5 \
     --num_train_epochs 3 \
     --plot_loss \
-    --lora_rank 16 \
+    --lora_rank 64 \
     --lora_dropout 0.15 \
     --lora_target query_key_value \
-    --bf16
+    --bf16 \
+    --resume_lora_training True
     
