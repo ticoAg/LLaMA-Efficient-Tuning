20c41fbe653885a0c8b72219c267162d41ad0f46
tico <1627635056@qq.com>
2023-07-18 13:54:27 +0800

    add ds pt baichuan7b test script

---
+++ scripts/enn/ds_full_train_baichuan.sh
@@ -0,0 +1,23 @@
+deepspeed --num_gpus=4 \
+    src/train_pt.py \
+    --model_name_or_path baichuan-inc/baichuan-7B \
+    --do_train \
+    --dataset medical \
+    --finetuning_type full \
+    --output_dir output/baichuan/full-tuning-7b-med \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 8 \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --save_steps 1000 \
+    --eval_steps 500 \
+    --learning_rate 5e-5 \
+    --max_grad_norm 0.5 \
+    --num_train_epochs 3.0 \
+    --dev_ratio 0.01 \
+    --evaluation_strategy steps \
+    --load_best_model_at_end \
+    --plot_loss \
+    --fp16 \
+    --deepspeed scripts/enn/ds_config.json
