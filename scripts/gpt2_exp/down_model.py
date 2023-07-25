b61a119a44b60bea760b37e1f9fd86a9a2caef1a
ticoAg <1627635056@qq.com>
2023-07-25 20:58:32 +0800

    update download dataset

--- scripts/gpt2_exp/down_model.py
+++ scripts/gpt2_exp/down_model.py
@@ -1,8 +1,16 @@
-from huggingface_hub import snapshot_download
-repo_id ="gpt2"
-snapshot_download(repo_id, 
-                  resume_download=True, # 中断根据缓存继续下载
-                  ignore_patterns=["*.safetensors", "*.msgpack","*.h5", "*.ot", ],
-                  allow_patterns=["*.model", "*.json", "*.bin","*.py", "*.md", "*.txt"],
-                #   proxies={"http":"http://127.0.0.1:7890", "https":"http://127.0.0.1:7890"}
-                  )
+# from huggingface_hub import snapshot_download
+# repo_id ="gpt2"
+# snapshot_download(repo_id, 
+#                   resume_download=True, # 中断根据缓存继续下载
+#                   ignore_patterns=["*.safetensors", "*.msgpack","*.h5", "*.ot", ],
+#                   allow_patterns=["*.model", "*.json", "*.bin","*.py", "*.md", "*.txt"],
+#                 #   proxies={"http":"http://127.0.0.1:7890", "https":"http://127.0.0.1:7890"}
+#                   )
+
+import datasets
+repo_id = "TigerResearch/pretrain_zh"
+config = datasets.DownloadConfig(resume_download=True, max_retries=1000, num_proc=8)
+dataset = datasets.load_dataset(repo_id, 
+                                    cache_dir=".hf_cache", 
+                                    download_config=config)
+dataset.save_to_disk(repo_id)
