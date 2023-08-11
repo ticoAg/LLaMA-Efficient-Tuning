4142a5bd7020a14108cc6566b9ff71dcc9cc5c10
ticoAg <1627635056@qq.com>
2023-08-11 18:16:43 +0800

    update gpt2 script

--- scripts/gpt2_exp/train.sh
+++ scripts/gpt2_exp/train.sh
@@ -5,8 +5,9 @@ CUDA_VISIBLE_DEVICES=1 python src/train_bash.py \
     --finetuning_type full \
     --dataset tiger_pretrain_zh \
     --template ziya \
+    --use_fast_tokenizer \
     --preprocessing_num_workers 64 \
-    --per_device_train_batch_size 4 \
+    --per_device_train_batch_size 8 \
     --gradient_accumulation_steps 16 \
     --output_dir .cache/gpt2-tigerResearch_pretrain_zh \
     --lr_scheduler_type cosine \
@@ -16,7 +17,7 @@ CUDA_VISIBLE_DEVICES=1 python src/train_bash.py \
     --learning_rate 5e-5 \
     --max_grad_norm 0.5 \
     --num_train_epochs 1.0 \
-    --dev_ratio 0.01 \
+    --val_size 0.01 \
     --evaluation_strategy steps \
     --plot_loss \
     --max_source_length 1024 \
