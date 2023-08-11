4056e10549c33dfd040126361728c411cab7ae1d
ticoAg <1627635056@qq.com>
2023-08-11 16:36:18 +0800

    update train script

--- scripts/gpt2_exp/train.sh
+++ scripts/gpt2_exp/train.sh
@@ -1,1 +1,23 @@
-python src/train_bash.py --stage pt --model_name_or_path gpt2 --do_train --dataset tigerResearch_pretrain_zh --finetuning_type full --output_dir .cache/gpt2-tigerResearch_pretrain_zh --per_device_train_batch_size 4 --gradient_accumulation_steps 16 --lr_scheduler_type cosine --logging_steps 10 --save_steps 1000 --eval_steps 500 --learning_rate 5e-5 --max_grad_norm 0.5 --num_train_epochs 1.0 --dev_ratio 0.01 --evaluation_strategy steps --plot_loss --preprocessing_num_workers 64 --max_source_length 1024 --bf16
+CUDA_VISIBLE_DEVICES=1 python src/train_bash.py \
+    --stage pt \
+    --model_name_or_path gpt2 \
+    --do_train \
+    --finetuning_type full \
+    --dataset tiger_pretrain_zh \
+    --template ziya \
+    --preprocessing_num_workers 64 \
+    --per_device_train_batch_size 4 \
+    --gradient_accumulation_steps 16 \
+    --output_dir .cache/gpt2-tigerResearch_pretrain_zh \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --save_steps 1000 \
+    --eval_steps 500 \
+    --learning_rate 5e-5 \
+    --max_grad_norm 0.5 \
+    --num_train_epochs 1.0 \
+    --dev_ratio 0.01 \
+    --evaluation_strategy steps \
+    --plot_loss \
+    --max_source_length 1024 \
+    --bf16
