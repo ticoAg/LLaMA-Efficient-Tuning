b3ade0d1980e504608639db87b602d61b54a23e3
ticoAg <1627635056@qq.com>
2023-08-16 21:40:53 +0800

    .

--- scripts/gpt2_exp/train.sh
+++ scripts/gpt2_exp/train.sh
@@ -3,16 +3,14 @@ CUDA_VISIBLE_DEVICES=1 python src/train_bash.py \
     --model_name_or_path gpt2 \
     --do_train \
     --finetuning_type full \
-    --dataset tiger_pretrain_zh \
+    --dataset alpaca_zh \
     --template ziya \
-    --streaming \
     --use_fast_tokenizer \
     --per_device_train_batch_size 16 \
     --gradient_accumulation_steps 16 \
-    --output_dir .cache/gpt2-tigerResearch_pretrain_zh \
+    --output_dir .cache/gpt2-alpaca_zh \
     --lr_scheduler_type cosine \
     --logging_steps 10 \
-    --max_steps 64453 \
     --save_steps 5000 \
     --eval_steps 5000 \
     --learning_rate 5e-5 \
