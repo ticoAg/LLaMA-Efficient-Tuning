e276ce9e14f710349e936fec22af58db79723d25
ticoAg <1627635056@qq.com>
2023-08-16 20:16:04 +0800

    update baichuan sft offical script

---
+++ scripts/full_tune_baichuan_offical/train.sh
@@ -0,0 +1,25 @@
+deepspeed --num_gpus=8 src/train_bash.py \
+    --stage sft \
+    --model_name_or_path baichuan-inc/Baichuan-13B-Base \
+    --do_train \
+    --dataset alpaca_gpt4_en,alpaca_gpt4_zh \
+    --finetuning_type full \
+    --output_dir .cache/baichuan_sft_offical \
+    --overwrite_cache \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 8 \
+    --preprocessing_num_workers 16 \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --save_steps 100 \
+    --eval_steps 100 \
+    --learning_rate 5e-5 \
+    --max_grad_norm 0.5 \
+    --num_train_epochs 2.0 \
+    --val_size 0.01 \
+    --evaluation_strategy steps \
+    --load_best_model_at_end \
+    --plot_loss \
+    --fp16 \
+    --deepspeed deepspeed.json
