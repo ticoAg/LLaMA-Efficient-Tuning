6b8385a1b7511de18ab660c9f1f12f2910de7e89
ticoAg <1627635056@qq.com>
2023-08-22 15:11:38 +0800

    .

--- scripts/pt_med/train.sh
+++ scripts/pt_med/train.sh
@@ -13,8 +13,10 @@ deepspeed --include localhost:4,5,6,7 \
         --gradient_accumulation_steps 8 \
         --preprocessing_num_workers 64 \
         --num_train_epochs 2.0 \
-    --save_steps 1000 \
-    --eval_steps 1000 \
+    --save_steps 500 \
+    --save_total_limit 5 \
+    --eval_steps 500 \
+    --load_best_model_at_end \
     --val_size 0.001 \
     --warmup_ratio 0.1 \
     --evaluation_strategy steps \
@@ -22,35 +24,6 @@ deepspeed --include localhost:4,5,6,7 \
         --lr_scheduler_type cosine \
         --max_grad_norm 0.5 \
     --logging_steps 10 \
-    --load_best_model_at_end \
     --plot_loss \
     --bf16 \
     --deepspeed scripts/ds_config/ds_stage2.json
-
-
-# deepspeed --num_gpus=8 src/train_bash.py \
-#     --stage sft \
-#     --model_name_or_path baichuan-inc/Baichuan-7B \
-#     --do_train \
-#     --dataset alpaca_gpt4_en,alpaca_gpt4_zh,self_cognition,firefly,oaast_sft_zh,sharegpt_zh \
-#     --finetuning_type full \
-#     --output_dir .cache/baichuan_sft_offical \
-#     --per_device_train_batch_size 8 \
-#     --per_device_eval_batch_size 8 \
-#     --gradient_accumulation_steps 4 \
-#     --preprocessing_num_workers 16 \
-#     --lr_scheduler_type cosine \
-#     --logging_steps 10 \
-#     --save_steps 2000 \
-#     --eval_steps 200 \
-#     --learning_rate 5e-5 \
-#     --max_grad_norm 0.5 \
-#     --num_train_epochs 2.0 \
-#     --val_size 0.01 \
-#     --evaluation_strategy steps \
-#     --load_best_model_at_end \
-#     --plot_loss \
-#     --template baichuan \
-#     --bf16 \
-#     --run_name baichuan_sft_offical_test \
-#     --deepspeed scripts/full_tune_baichuan_offical/deep_speed.json
