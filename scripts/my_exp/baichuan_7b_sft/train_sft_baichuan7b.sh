e68add718ef559764227f21fd7643ed821e6ddc6
ticoAg <1627635056@qq.com>
2023-09-01 22:10:45 +0800

    .

--- scripts/my_exp/baichuan_7b_sft/train_sft_baichuan7b.sh
+++ scripts/my_exp/baichuan_7b_sft/train_sft_baichuan7b.sh
@@ -10,7 +10,7 @@ deepspeed --include localhost:0,1,2,3,4,5,6,7 \
         --dataset sft_med_multiturn \
         --max_source_length 2048 \
         --per_device_train_batch_size 8 \
-        --per_device_eval_batch_size 16 \
+        --per_device_eval_batch_size 8 \
         --gradient_accumulation_steps 16 \
         --preprocessing_num_workers 64 \
         --use_fast_tokenizer True \
