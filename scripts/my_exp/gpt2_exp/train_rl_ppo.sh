b2328a586991ae5c80314ba598c8ab611d45c2ba
ticoAg <1627635056@qq.com>
2023-09-06 23:33:57 +0800

    update gpt2 ppo script

--- scripts/my_exp/gpt2_exp/train_rl_ppo.sh
+++ scripts/my_exp/gpt2_exp/train_rl_ppo.sh
@@ -1,20 +1,50 @@
-CUDA_VISIBLE_DEVICES=0 python src/train_bash.py \
+export WANDB_PROJECT=gpt2-proj
+export CUDA_VISIBLE_DEVICES=0
+
+deepspeed --include localhost:0,1,2,3,4,5,6,7 \
+    src/train_bash.py \
     --stage ppo \
     --model_name_or_path ticoAg/gpt2-tiger-sft-zh \
+    --overwrite_output_dir \
     --do_train \
     --dataset alpaca_gpt4_zh \
-    --template default \
+    --template ziya \
     --finetuning_type lora \
-    --lora_target q_proj,v_proj \
-    --resume_lora_training False \
-    --checkpoint_dir path_to_sft_checkpoint \
-    --reward_model path_to_rm_checkpoint \
-    --output_dir path_to_ppo_checkpoint \
-    --per_device_train_batch_size 2 \
-    --gradient_accumulation_steps 4 \
+    --lora_target c_proj \
+    --local_rank 128 \
+    --reward_model .cache/gpt2-tiger-zh-sft-rm \
+    --output_dir .cache/gpt2-tiger-zh-sft-ppo \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 8 \
     --lr_scheduler_type cosine \
-    --logging_steps 10 \
-    --save_steps 1000 \
+    --logging_steps 5 \
+    --save_steps 0.5 \
     --learning_rate 1e-5 \
-    --num_train_epochs 1.0 \
-    --plot_loss
+    --num_train_epochs 3.0 \
+    --run_name gpt2-tiger-zh-sft-rm \
+    --plot_loss \
+    --bf16
+
+# python src/train_bash.py ^
+#     --stage ppo ^
+#     --model_name_or_path ticoAg/gpt2-tiger-sft-zh ^
+#     --overwrite_output_dir ^
+#     --do_train ^
+#     --dataset alpaca_gpt4_zh ^
+#     --template ziya ^
+#     --finetuning_type lora ^
+#     --lora_target c_proj ^
+#     --reward_model .cache/gpt2-tiger-zh-sft-rm ^
+#     --output_dir .cache/gpt2-tiger-zh-sft-ppo ^
+#     --per_device_train_batch_size 2 ^
+#     --per_device_eval_batch_size 2 ^
+#     --gradient_accumulation_steps 1 ^
+#     --lr_scheduler_type cosine ^
+#     --logging_steps 1 ^
+#     --save_steps 0.5 ^
+#     --learning_rate 1e-5 ^
+#     --num_train_epochs 3.0 ^
+#     --run_name gpt2-tiger-zh-sft-ppo ^
+#     --plot_loss ^
+#     --bf16
