dea966b5225375989c0e5c9e8a28459292f1404c
ticoAg <1627635056@qq.com>
2023-09-07 11:14:20 +0800

    .

--- scripts/pt_med/llm-pretrain-med-2G-exp.005/train.sh
+++ scripts/pt_med/llm-pretrain-med-2G-exp.005/train.sh
@@ -2,12 +2,16 @@ export WANDB_PROJECT=huggingface
 
 exp_id=llm-pretrain-med-2G-exp.005
 model_name_or_path=THUDM/chatglm2-6b
-dataset=pretrain_med_v0.1_book_wiki_qaConcat,Wudao_health_subset
+dataset=Wudao_health_subset
 template=chatglm2
+gpu_vis=2,3,4,5,6,7
+MASTER_PORT=2345
+
 
 wandb online
-deepspeed --include localhost:0,1,2,3,4,5,6,7 \
+deepspeed  --include localhost:$gpu_vis --master_port $MASTER_PORT \
     src/train_bash.py \
+    --deepspeed scripts/ds_config/ds_stage2.json \
     --stage pt \
     --do_train \
     --finetuning_type full \
@@ -29,10 +33,9 @@ deepspeed --include localhost:0,1,2,3,4,5,6,7 \
     --warmup_ratio 0.1 \
     --evaluation_strategy steps \
         --learning_rate 5e-5 \
-        --lr_scheduler_type cosine \
+        --lr_scheduler_type co,sine \
         --max_grad_norm 0.5 \
     --logging_steps 10 \
     --plot_loss \
     --bf16 \
-    --run_name $exp_id \
-    --deepspeed scripts/ds_config/ds_stage2.json
+    --run_name $exp_id
