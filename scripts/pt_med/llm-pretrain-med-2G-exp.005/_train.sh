66867d825aba254413f54b386270092d1931bfc5
ticoAg <1627635056@qq.com>
2023-09-07 11:34:11 +0800

    .

--- scripts/pt_med/llm-pretrain-med-2G-exp.005/_train.sh
+++ scripts/pt_med/llm-pretrain-med-2G-exp.005/_train.sh
@@ -2,7 +2,8 @@ export WANDB_PROJECT=huggingface
 
 exp_id=llm-pretrain-med-2G-exp.test.005
 model_name_or_path=THUDM/chatglm2-6b
-dataset=pretrain_med_v0.1_book_wiki_qaConcat,Wudao_health_subset
+# dataset=pretrain_med_v0.1_book_wiki_qaConcat,Wudao_health_subset
+dataset=Wudao_health_subset
 template=chatglm2
 
 wandb offline
