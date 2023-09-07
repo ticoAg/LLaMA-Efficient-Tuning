e3911c421bc5091d520b59b2d3d325dc4cef44bb
ticoAg <1627635056@qq.com>
2023-09-07 10:57:45 +0800

    .

--- scripts/pt_med/llm-pretrain-med-2G-exp.005/_train.sh
+++ scripts/pt_med/llm-pretrain-med-2G-exp.005/_train.sh
@@ -19,7 +19,7 @@ deepspeed --include localhost:0,1,2,3,4,5,6,7 \
         --max_source_length 4096 \
         --per_device_train_batch_size 2 \
         --per_device_eval_batch_size 2 \
-        --gradient_accumulation_steps 8 \
+        --gradient_accumulation_steps 1 \
         --preprocessing_num_workers 64 \
         --use_fast_tokenizer True \
         --num_train_epochs 2.0 \
