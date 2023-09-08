16f3ab083eb329e78f1def671f702fb9371cf5b0
ticoAg <1627635056@qq.com>
2023-09-08 15:31:52 +0800

    baichuan7b_sft_multimed infer script

--- scripts/my_exp/baichuan_7b_sft/infer_sft_baichuan7b.sh
+++ scripts/my_exp/baichuan_7b_sft/infer_sft_baichuan7b.sh
@@ -1,4 +1,9 @@
-export CUDA_VISIBLE_DEVICES=0
-python src/web_demo.py \
+CUDA_VISIBLE_DEVICES=1 python src/web_demo.py \
   --model_name_or_path .cache/baichuan7b_sft_multimed \
-  --template baichuan
+  --template baichuan \
+  --do_sample \
+  --top_k 5 \
+  --temperature 0.3 \
+  --top_p 0.85 \
+  --repetition_penalty 1.05 \
+  --max_new_tokens 2048
