4d5acff6899ebcecd0d03abebcc6358c80d3f040
ticoAg <1627635056@qq.com>
2023-09-12 22:10:53 +0800

    .

--- scripts/my_exp/gpt2_exp/train_rl_rm.sh
+++ scripts/my_exp/gpt2_exp/train_rl_rm.sh
@@ -30,14 +30,11 @@ CUDA_VISIBLE_DEVICES=$gpu_vis python \
         --use_fast_tokenizer True \
         --num_train_epochs 2.0 \
     --save_strategy epoch \
-    --val_size 400 \
-    --eval_steps 200 \
-    --evaluation_strategy steps \
-    --warmup_ratio 0.1 \
-        --learning_rate 5e-5 \
+    --warmup_ratio 0.05 \
+        --learning_rate 1e-6 \
         --lr_scheduler_type cosine \
         --max_grad_norm 0.5 \
-        --adam_epsilon 5e-7 \
+        --adam_epsilon 1e-8 \
     --logging_steps 1 \
     --plot_loss \
     --bf16 \
@@ -75,25 +72,27 @@ CUDA_VISIBLE_DEVICES=$gpu_vis python \
 
 # python src/train_bash.py ^
 #     --stage rm ^
-#     --model_name_or_path ticoAg/gpt2-tiger-sft-zh ^
-#     --overwrite_output_dir ^
 #     --do_train ^
-#     --finetuning_type lora ^
-#     --lora_target c_proj ^
-#     --dataset comparison_gpt4_zh ^
-#     --preprocessing_num_workers 4 ^
-#     --use_fast_tokenizer ^
-#     --template ziya ^
+#     --finetuning_type full ^
+#     --model_name_or_path ticoAg/gpt2-tiger-sft-zh ^
 #     --output_dir .cache/gpt2-tiger-zh-sft-rm ^
-#     --per_device_train_batch_size 4 ^
-#     --per_device_eval_batch_size 4 ^
-#     --gradient_accumulation_steps 4 ^
-#     --lr_scheduler_type cosine ^
-#     --logging_steps 1 ^
-#     --save_steps 0.5 ^
-#     --learning_rate 1e-5 ^
+#     --overwrite_output_dir ^
+#         --template ziya ^
+#         --dataset comparison_gpt4_zh ^
+#         --max_source_length 1024 ^
+#         --per_device_train_batch_size 2 ^
+#         --per_device_eval_batch_size 2 ^
+#         --gradient_accumulation_steps 2 ^
+#         --preprocessing_num_workers 8 ^
+#         --use_fast_tokenizer True ^
+#         --num_train_epochs 2.0 ^
+#     --save_strategy epoch ^
 #     --warmup_ratio 0.1 ^
-#     --num_train_epochs 1.0 ^
+#         --learning_rate 1e-6 ^
+#         --lr_scheduler_type cosine ^
+#         --max_grad_norm 0.5 ^
+#         --adam_epsilon 1e-8 ^
+#     --logging_steps 1 ^
 #     --plot_loss ^
-#     --run_name gpt2-tiger-zh-sft-rm ^
-#     --bf16
+#     --bf16 ^
+#     --run_name gpt2-tiger-zh-sft-rm
