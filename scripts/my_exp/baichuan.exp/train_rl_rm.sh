77e05cc5629fb9b0b5f514c0c7c722edd5d13c51
ticoAg <1627635056@qq.com>
2023-09-12 22:48:46 +0800

    .

--- scripts/my_exp/baichuan.exp/train_rl_rm.sh
+++ scripts/my_exp/baichuan.exp/train_rl_rm.sh
@@ -1,9 +1,9 @@
-export WANDB_PROJECT=gpt2-proj
+export WANDB_PROJECT=huggingface
 
-exp_id=gpt2-tiger-zh-sft-rm
-model_name_or_path=ticoAg/gpt2-tiger-sft-zh
+exp_id=Baichuan2-13B-Base-RM
+model_name_or_path=/data/songhaoyang/LLaMA-Efficient-Tuning/.cache/Baichuan2-13B-Base-Sfted-Mixed
 dataset=comparison_gpt4_zh
-template=ziya
+template=baichuan2
 # gpu_vis=2,3,4,5,6,7
 gpu_vis=0
 MASTER_PORT=2346
@@ -17,7 +17,7 @@ CUDA_VISIBLE_DEVICES=$gpu_vis python \
     --stage rm \
     --do_train \
     --finetuning_type lora \
-    --lora_target c_proj \
+    --lora_target W_pack \
     --lora_rank 64 \
     --resume_lora_training False \
     --model_name_or_path $model_name_or_path \
@@ -25,7 +25,7 @@ CUDA_VISIBLE_DEVICES=$gpu_vis python \
     --overwrite_output_dir \
         --template $template \
         --dataset $dataset \
-        --max_source_length 1024 \
+        --max_source_length 4096 \
         --per_device_train_batch_size 2 \
         --gradient_accumulation_steps 8 \
         --preprocessing_num_workers 8 \
