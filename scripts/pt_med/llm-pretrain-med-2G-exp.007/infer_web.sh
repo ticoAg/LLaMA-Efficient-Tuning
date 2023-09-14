218d5f35d274034d86ef8022a0cd1e6b3a199c28
ticoAg <1627635056@qq.com>
2023-09-14 15:23:59 +0800

    .

--- scripts/pt_med/llm-pretrain-med-2G-exp.007/infer_web.sh
+++ scripts/pt_med/llm-pretrain-med-2G-exp.007/infer_web.sh
@@ -1,10 +1,11 @@
 export WANDB_PROJECT=huggingface
 
-exp_id=llm-pretrain-med-2G-exp.006
-model_name_or_path=baichuan-inc/Baichuan2-13B-Base
+exp_id=llm-pretrain-med-2G-exp.007
+model_name_or_path=THUDM/chatglm2-6b-32k
 dataset=pretrain_med_v0.1_book_wiki_qaConcat,Wudao_health_subset
-template=baichuan2
-gpu_vis=2,3,4,5,6,7
+template=chatglm2
+# gpu_vis=2,3,4,5,6,7
+gpu_vis=1
 MASTER_PORT=2345
 
 CUDA_VISIBLE_DEVICES=0 python src/web_demo.py \
