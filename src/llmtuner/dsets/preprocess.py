1bd7adfd28fa0aa86e25b22ee95d6052e93c2e8a
ticoAg <1627635056@qq.com>
2023-09-14 11:54:36 +0800

    debug ppo ds

--- src/llmtuner/dsets/preprocess.py
+++ src/llmtuner/dsets/preprocess.py
@@ -115,7 +115,8 @@ def preprocess_dataset(
         model_inputs = {"input_ids": [], "attention_mask": [], "labels": []}
 
         for query, response, history, system in construct_example_ori(examples):
-            print(examples)
+            for i in [query, response, history, system]:
+                print(i)
             source_ids, target_ids = template.encode_oneturn(tokenizer, query, response, history, system)
 
             if len(source_ids) > data_args.max_source_length:
