01ba8d85eeae38b0672b103badbccc793cc67c25
songhaoyanga <songhaoyanga@enn.cn>
2023-09-15 18:05:20 +0800

    .

--- scripts/baichuan.exp/v2/export_baichuan2_13b_ppo_v2.sh
+++ scripts/baichuan.exp/v2/export_baichuan2_13b_ppo_v2.sh
@@ -1,8 +1,9 @@
 export WANDB_PROJECT=huggingface
 
-exp_id=Baichuan2-13B-Base-Sfted-Mixed-PPO-Exported-V1
-model_name_or_path=.cache/Baichuan2-13B-Base-Sfted-Mixed
-reward_model=.cache/Baichuan2-13B-Base-RM
+root_dir=.cache
+exp_id=Baichuan2-13B-Base-Sfted-Mixed-PPO-Exported-V2
+model_name_or_path=Baichuan2-13B-Base-Sfted-Mixed
+reward_model=Baichuan2-13B-Base-RM-V2
 dataset=alpaca_gpt4_zh
 template=baichuan2
 gpu_vis=0,1,2,3,4,5
@@ -12,7 +13,7 @@ acclerate_config=scripts/acc_config/default_config.yaml
 
 wandb offline
 python src/export_model.py \
-    --model_name_or_path $model_name_or_path \
+    --model_name_or_path $root_dir/$model_name_or_path \
     --template $template \
     --finetuning_type lora \
     --checkpoint_dir .cache/Baichuan2-13B-Base-Sfted-Mixed-PPO-V1 \
--- scripts/baichuan.exp/v2/train_baichuan2_13b_ppo_v2.sh
+++ scripts/baichuan.exp/v2/train_baichuan2_13b_ppo_v2.sh
@@ -1,44 +1,49 @@
 export WANDB_PROJECT=huggingface
 
-exp_id=Baichuan2-13B-Base-RM-V2
-model_name_or_path=baichuan-inc/Baichuan2-13B-Base
-dataset=comparison_gpt4_zh
+root_dir=.cache
+exp_id=Baichuan2-13B-Base-Sfted-Mixed-PPO-V2
+model_name_or_path=Baichuan2-13B-Base-Sfted-Mixed
+reward_model=Baichuan2-13B-Base-RM-V2
+dataset=alpaca_gpt4_zh
 template=baichuan2
 gpu_vis=0,1,2,3,4,5
 # gpu_vis=0
-MASTER_PORT=2346
+# MASTER_PORT=2346
+acclerate_config=scripts/acc_config/default_config.yaml
 
 
 wandb online
 # wandb offline
+
 # CUDA_VISIBLE_DEVICES=$gpu_vis python \
-deepspeed --include localhost:$gpu_vis --master_port $MASTER_PORT \
-    src/train_bash.py \
-    --stage rm \
+# deepspeed  --include localhost:$gpu_vis --master_port $MASTER_PORT \
+CUDA_VISIBLE_DEVICES=$gpu_vis accelerate launch --config_file $acclerate_config src/train_bash.py \
+    --stage ppo \
     --do_train \
     --finetuning_type lora \
     --lora_target W_pack \
     --lora_rank 64 \
     --resume_lora_training False \
-    --model_name_or_path $model_name_or_path \
-    --output_dir .cache/$exp_id \
+    --model_name_or_path $root_dir/$model_name_or_path \
+    --reward_model $root_dir/$reward_model \
+    --output_dir $root_dir/$exp_id \
     --overwrite_output_dir \
         --template $template \
         --dataset $dataset \
         --max_source_length 4096 \
         --per_device_train_batch_size 2 \
-        --gradient_accumulation_steps 8 \
+        --gradient_accumulation_steps 2 \
         --preprocessing_num_workers 128 \
-        --num_train_epochs 2.0 \
+        --num_train_epochs 1 \
     --save_strategy epoch \
     --warmup_ratio 0.05 \
-        --learning_rate 1e-6 \
+        --learning_rate 1e-5 \
         --lr_scheduler_type cosine \
         --max_grad_norm 0.5 \
-        --adam_epsilon 1e-8 \
-    --logging_steps 1 \
+        --adam_epsilon 1e-7 \
+    --logging_steps 5 \
     --plot_loss \
-    --fp16 \
+    --bf16 \
     --run_name $exp_id
     # --deepspeed scripts/ds_config/ds_stage2.json
 
