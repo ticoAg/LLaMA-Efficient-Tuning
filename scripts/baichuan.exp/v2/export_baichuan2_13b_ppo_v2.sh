695cd25c2eb95e5ead185582ad1ccee6e16aa7c1
songhaoyanga <songhaoyanga@enn.cn>
2023-09-18 09:55:41 +0800

    export

--- scripts/baichuan.exp/v2/export_baichuan2_13b_ppo_v2.sh
+++ scripts/baichuan.exp/v2/export_baichuan2_13b_ppo_v2.sh
@@ -3,18 +3,13 @@ export WANDB_PROJECT=huggingface
 root_dir=.cache
 exp_id=Baichuan2-13B-Base-Sfted-Mixed-PPO-Exported-V2
 model_name_or_path=Baichuan2-13B-Base-Sfted-Mixed
-reward_model=Baichuan2-13B-Base-RM-V2
-dataset=alpaca_gpt4_zh
+lora_ckpt_model=Baichuan2-13B-Base-Sfted-Mixed-PPO-V2
 template=baichuan2
-gpu_vis=0,1,2,3,4,5
-# gpu_vis=0
-# MASTER_PORT=2346
-acclerate_config=scripts/acc_config/default_config.yaml
 
 wandb offline
 python src/export_model.py \
     --model_name_or_path $root_dir/$model_name_or_path \
     --template $template \
     --finetuning_type lora \
-    --checkpoint_dir .cache/Baichuan2-13B-Base-Sfted-Mixed-PPO-V1 \
+    --checkpoint_dir $root_dir/$lora_ckpt_model \
     --output_dir $exp_id
