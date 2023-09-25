5d547f1b719abcd1aabf25d233ee049b2403c140
songhaoyanga <songhaoyanga@enn.cn>
2023-09-25 14:51:27 +0800

    ppo_v2 new rm alpaca_gpt4_zh

--- scripts/baichuan.exp/v2/train_baichuan2_13b_ppo_v2.sh
+++ scripts/baichuan.exp/v2/train_baichuan2_13b_ppo_v2.sh
@@ -4,20 +4,15 @@ proj_dir=.cache/baichuan.exp
 root_dir=.cache/baichuan.exp/v2
 exp_id=Baichuan2-13B-Base-Sfted-Mixed-PPO-V2
 model_name_or_path=Baichuan2-13B-Base-Sfted-Mixed
-reward_model=Baichuan2-13B-Base-RM-V2
+reward_model=Baichuan2-13B-Base-RM-HH-RLHF
 dataset=alpaca_gpt4_zh
 template=baichuan2
 gpu_vis=0,1,2,3,4,5
-# gpu_vis=0
-# MASTER_PORT=2346
 acclerate_config=scripts/acc_config/default_config.yaml
-
 
 wandb online
 # wandb offline
 
-# CUDA_VISIBLE_DEVICES=$gpu_vis python \
-# deepspeed  --include localhost:$gpu_vis --master_port $MASTER_PORT \
 CUDA_VISIBLE_DEVICES=$gpu_vis accelerate launch --config_file $acclerate_config src/train_bash.py \
     --stage ppo \
     --do_train \
@@ -26,7 +21,7 @@ CUDA_VISIBLE_DEVICES=$gpu_vis accelerate launch --config_file $acclerate_config 
     --lora_rank 64 \
     --resume_lora_training False \
     --model_name_or_path $proj_dir/$model_name_or_path \
-    --reward_model $root_dir/$reward_model \
+    --reward_model $proj_dir/rm/$reward_model \
     --output_dir $root_dir/$exp_id \
     --overwrite_output_dir \
         --template $template \
@@ -35,14 +30,15 @@ CUDA_VISIBLE_DEVICES=$gpu_vis accelerate launch --config_file $acclerate_config 
         --per_device_train_batch_size 2 \
         --gradient_accumulation_steps 2 \
         --preprocessing_num_workers 128 \
-        --num_train_epochs 2 \
+        --num_train_epochs 3 \
     --save_strategy epoch \
-    --warmup_ratio 0.05 \
+    --warmup_ratio 0.1 \
         --learning_rate 1e-5 \
         --lr_scheduler_type cosine \
         --max_grad_norm 0.5 \
         --adam_epsilon 1e-7 \
     --logging_steps 5 \
+    --flash_attn \
     --plot_loss \
     --bf16 \
     --run_name $exp_id
