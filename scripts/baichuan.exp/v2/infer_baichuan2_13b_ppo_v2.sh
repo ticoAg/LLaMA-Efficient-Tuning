ad591b9cd7ae601464cced448876fec9784c7fc9
songhaoyanga <songhaoyanga@enn.cn>
2023-09-28 13:43:43 +0800

    update Baichuan2-13B-Base-Sfted-Mixed-PPO-V2 infer

---
+++ scripts/baichuan.exp/v2/infer_baichuan2_13b_ppo_v2.sh
@@ -0,0 +1,22 @@
+export WANDB_PROJECT=huggingface
+
+proj_dir=.cache/baichuan.exp
+root_dir=.cache/baichuan.exp/v2
+exp_id=Baichuan2-13B-Base-Sfted-Mixed-PPO-V2
+model_name_or_path=Baichuan2-13B-Base-Sfted-Mixed
+reward_model=Baichuan2-13B-Base-RM-HH-RLHF
+dataset=alpaca_gpt4_zh
+template=baichuan2
+
+
+CUDA_VISIBLE_DEVICES=0 python src/web_demo.py \
+  --model_name_or_path $proj_dir/$model_name_or_path \
+  --checkpoint_dir $root_dir/$exp_id \
+  --finetuning_type lora \
+  --template baichuan2 \
+  --do_sample \
+  --top_k 5 \
+  --temperature 0.3 \
+  --top_p 0.85 \
+  --repetition_penalty 1.05 \
+  --cutoff_len 4096
--- src/web_demo.py
+++ src/web_demo.py
@@ -4,7 +4,7 @@ from llmtuner import create_web_demo
 def main():
     demo = create_web_demo()
     demo.queue()
-    demo.launch(server_name="0.0.0.0", server_port=26921, share=False, inbrowser=False)
+    demo.launch(server_name="0.0.0.0", server_port=26926, share=False, inbrowser=False)
 
 
 if __name__ == "__main__":
