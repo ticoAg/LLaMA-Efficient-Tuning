d50a8db828a036f0b69fbf086d726ee3594b54c9
ticoAg <1627635056@qq.com>
2024-03-22 20:45:59 +0800

    .

--- scripts/dpo_exp/qwen1.5-0.5/sft.sh
+++ scripts/dpo_exp/qwen1.5-0.5/sft.sh
@@ -7,42 +7,41 @@ export https_proxy=http://127.0.0.1:7890
 export HTTPS_PROXY=http://127.0.0.1:7890
 export all_proxy=http://127.0.0.1:7890
 export ALL_PROXY=http://127.0.0.1:7890
-export CUDA_VISIBLE_DEVICES=5,6,7
+# export CUDA_VISIBLE_DEVICES=0
 
 # CUDA_VISIBLE_DEVICES=5,6,7 python /data/songhaoyang/LLaMA-Efficient-Tuning/src/train_bash.py \
-# CUDA_VISIBLE_DEVICES=5,6,7 accelerate launch --config_file /data/songhaoyang/LLaMA-Efficient-Tuning/scripts/dpo_exp/qwen1.5-0.5/config.yaml /data/songhaoyang/LLaMA-Efficient-Tuning/src/train_bash.py \
-accelerate launch --config_file /data/songhaoyang/LLaMA-Efficient-Tuning/scripts/dpo_exp/qwen1.5-0.5/config.yaml /data/songhaoyang/LLaMA-Efficient-Tuning/src/train_bash.py \
+accelerate launch --config_file scripts/dpo_exp/qwen1.5-0.5/config.yaml src/train_bash.py \
     --stage sft \
     --do_train \
     --model_name_or_path qwen/Qwen1.5-0.5B \
-    --dataset alpaca_gpt4_en,alpaca_gpt4_zh,oaast_sft_zh,firefly \
-    --dataset_dir /data/songhaoyang/LLaMA-Efficient-Tuning/data \
+    --dataset alpaca_gpt4_en,alpaca_gpt4_zh \
+    --dataset_dir ./data \
     --template qwen \
     --finetuning_type lora \
     --lora_rank 128 \
     --lora_target q_proj,v_proj \
-    --output_dir /data/songhaoyang/LLaMA-Efficient-Tuning/scripts/dpo_exp/qwen1.5-0.5/sft_ckpt \
+    --output_dir scripts/dpo_exp/qwen1.5-0.5/sft_ckpt \
     --use_fast_tokenizer \
     --overwrite_output_dir \
     --cutoff_len 4096 \
     --preprocessing_num_workers 8 \
-    --per_device_train_batch_size 8 \
-    --per_device_eval_batch_size 8 \
-    --gradient_accumulation_steps 128 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 32 \
     --lr_scheduler_type cosine \
     --logging_steps 1 \
     --save_steps 100 \
     --eval_steps 100 \
     --evaluation_strategy steps \
     --load_best_model_at_end \
-    --learning_rate 1e-5 \
+    --learning_rate 5e-5 \
     --lr_scheduler_type cosine \
-    --num_train_epochs 1.0 \
+    --num_train_epochs 3.0 \
     --warmup_ratio 0.1 \
     --val_size 0.05 \
     --fp16 \
     --report_to wandb \
-    --run_name qwen1.5-0.5B-sft-alpaca-oaast-firefly \
+    --run_name qwen1.5-0.5B-sft-alpacagpt4 \
     --ddp_find_unused_parameters False
 
 
